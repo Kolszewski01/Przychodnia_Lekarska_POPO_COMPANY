0d5056ae36700d2b81092531cb1ff5d90ef96ea2
Konrad Olszewski <92027283+Kolszewski01@users.noreply.github.com>
2024-01-14 20:47:24 +0100

    1/14/2024 8:47

--- PrzychodniaApi/app/admin_panel/models.py
+++ PrzychodniaApi/app/admin_panel/models.py
@@ -1,26 +1,58 @@
-# from django.db import models
-# from user.models import User
-# from worker.models import Doctor, Secretary
-# from django.db import models
-#
-# class Admin(User):
-#     def add_doctor(self, first_name, last_name, specialization, room_number, phone_number):
-#         doctor = Doctor(
-#             first_name=first_name,
-#             last_name=last_name,
-#             specialization=specialization,
-#             room_number=room_number,
-#             phone_number=phone_number
-#         )
-#         doctor.save()
-#
-#     def add_secretary(self, first_name, last_name, role, working_hours, office_phone):
-#         secretary = Secretary(
-#             first_name=first_name,
-#             last_name=last_name,
-#             role=role,
-#             working_hours=working_hours,
-#             office_phone=office_phone
-#         )
-#         secretary.save()
-#
+from django.db import models
+from user.models import User
+from worker.models import Doctor, Secretary
+
+class Admin(User):
+    def add_doctor(self, email, password, first_name, last_name, phone_number, city, street, house_number, zip_code, specialization, room_number, prof_title,employment_status, **extra_fields):
+
+        doctor = Doctor(
+            email=email, first_name=first_name, last_name=last_name,
+            phone_number=phone_number, city=city, street=street,
+            house_number=house_number, zip_code=zip_code,
+            specialization=specialization, room_number=room_number,
+            prof_title=prof_title,employment_status=employment_status, **extra_fields
+        )
+        doctor.set_password(password)
+        doctor.save()
+        return doctor
+
+    def add_secretary(self, email, password, first_name, last_name, phone_number, city, street, house_number, zip_code, **extra_fields):
+
+        secretary = Secretary(
+            email=email, first_name=first_name, last_name=last_name,
+            phone_number=phone_number, city=city, street=street,
+            house_number=house_number, zip_code=zip_code,
+            **extra_fields
+        )
+        secretary.set_password(password)
+        secretary.save()
+        return secretary
+
+    def remove_worker(self, worker_id, worker_type):
+        """
+        Usuwa pracownika (lekarza lub sekretarkę) na podstawie ID.
+        """
+        if worker_type == 'doctor':
+            model = Doctor
+        elif worker_type == 'secretary':
+            model = Secretary
+        else:
+            raise ValueError("Nieprawidłowy typ pracownika: 'doctor' lub 'secretary'")
+
+        model.objects.filter(id=worker_id).delete()
+
+    def modify_worker(self, worker_id, worker_type, **updated_fields):
+        """
+        Modyfikuje dane pracownika (lekarza lub sekretarki).
+        """
+        if worker_type == 'doctor':
+            model = Doctor
+        elif worker_type == 'secretary':
+            model = Secretary
+        else:
+            raise ValueError("Nieprawidłowy typ pracownika: 'doctor' lub 'secretary'")
+
+        worker = model.objects.get(id=worker_id)
+        for field, value in updated_fields.items():
+            setattr(worker, field, value)
+        worker.save()
--- PrzychodniaApi/app/worker/models.py
+++ PrzychodniaApi/app/worker/models.py
@@ -3,13 +3,13 @@ from user.models import User
 
 
 class Worker(User):
-    EMPLOYMENT_STATUS_CHOICES = [
-        ('active', 'Active'),
-        ('on_leave', 'On Leave'),
-        ('inactive', 'Inactive'),
+    EMPLOYMENT_STATUS_CHOICES  = [
+        ('aktywny', 'Aktywny'),
+        ('na_urlopie', 'Na Urlopie'),
+        ('nieaktywny', 'Nieaktywny'),
     ]
 
-    employment_status = models.CharField(max_length=10, choices=EMPLOYMENT_STATUS_CHOICES, default='active')
+    employment_status = models.CharField(choices=EMPLOYMENT_STATUS_CHOICES, default='Aktywny')
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
     phone_number = models.CharField(max_length=15)
@@ -24,8 +24,30 @@ class Worker(User):
 
 
 class Doctor(Worker):
+    PROF_TITLE_CHOICES = [
+        ('MD', 'Doktor medycyny'),
+        ('PhD', 'Doktor nauk medycznych'),
+        ('Prof.', 'Profesor'),
+        ('Dr hab. med.', 'Doktor habilitowany medycyny'),
+        ('Spec.', 'Specjalista'),
+        ('Asystent', 'Młodszy lekarz'),
+        ('Doc.', 'Docent'),
+        ('Doc. hab.', 'Doktor habilitowany'),
+        ('Doc. dr', 'Doktor habilitowany nauk medycznych'),
+        ('Dr. med.', 'Doktor medycyny'),
+        ('Kier. lab.', 'Kierownik laboratorium'),
+        ('Prof. dr hab. med.', 'Profesor doktor habilitowany medycyny'),
+        ('Doc. dr hab. med.', 'Docent doktor habilitowany medycyny'),
+        ('Dr n. med.', 'Doktor nauk medycznych'),
+        ('Dr inż.', 'Doktor inżynier'),
+        ('Dr med. vet.', 'Doktor medycyny weterynaryjnej'),
+        ('Dr dent.', 'Doktor stomatologii')
+    ]
+
     specialization = models.CharField(max_length=120)
     room_number = models.CharField(max_length=10, verbose_name='Room Number')
+    prof_title = models.CharField(choices=PROF_TITLE_CHOICES, default='Dr. med.')
+
 
 class Secretary(Worker):
     pass
